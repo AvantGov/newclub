6964422cd097eb260d3e3aa05cea0acee784b171
avantgov <alexcouts18@gmail.com>
2022-08-02 21:49:49 -0500

    initial dev commit with some work done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
+// * DEPENDS 
+import React, {useEffect} from 'react';
+import Display from './Components/Display';
+// * CSS 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// * MODS
+const Hydra = require('hydra-synth');
 
+
+function App() {
+  // creating background
+  var texture = document.createElement("canvas")
+  texture.setAttribute("class","texture")
+  texture.setAttribute("id","txtr_main")
+
+  // init module 
+  const createHydra = (context) => {
+    const hydra = new Hydra({
+      detectAudio: false,
+      canvas: texture,
+      makeGlobal: false
+    }).synth
+    hydra.gradient(1).out()
+  }
+
+  // add the module output after DOM rendering
+  useEffect(() => {
+    createHydra()
+    const A = document.getElementById("App")
+    A.insertBefore(texture, A.childNodes[0])
+  },[texture])
+
+  return (
+    <div id="App" className="App">
+      <Display />
     </div>
   );
 }
---
+++ client/src/Components/Home.js
@@ -0,0 +1,13 @@
+// DEPENDS.
+import React from 'react';
+
+const Home = () => {
+
+    return(
+        <div className='Home'>
+            Hello you are on the homepage
+        </div>
+    );
+};
+
+export default Home;
